8feb81fceb68d36fbb2a8d7da53964be34080868
Indra Putra <indrakoslab@gmail.com>
2018-07-20 17:37:00 +0700

    simplified code for level 04

--- level04/index.js
+++ level04/index.js
@@ -1,6 +1,6 @@
-async function beforePromise() {
-    return "Before Promise"
-}
+// async function beforePromise() {
+//     return "Before Promise"
+// }
 
 const inputIsNumber = (input) => {
     if (typeof input === "number") {
@@ -18,17 +18,25 @@ const multipleByThree = (result) => {
     return Promise.resolve(result * 3);
 }
 
-async function afterPromise() {
-    let promise = new Promise((resolve) => {
-        setTimeout(() => resolve("After Promise"), 1)
-    });
-    let result = await promise
-    console.log(result)
-};
+// async function afterPromise() {
+//     let promise = new Promise((resolve) => {
+//         setTimeout(() => resolve("After Promise"), 1)
+//     });
+//     let result = await promise
+//     console.log(result)
+// };
 
+// new version
+async function thisSync() {
+   await console.log("Before Promise");
+   await inputIsNumber(2).then(multipleByTwo).then(multipleByTwo).then(multipleByThree).then(res => console.log(res)).catch(err => console.log(err));
+   await console.log("After Promise");
+}
+
+thisSync()
 
 // inputIsNumber().then(multipleByTwo).then(multipleByTwo).then(multipleByThree).then(res => console.log(res)).catch(err => console.log(err));
-beforePromise().then(res => console.log(res));
-inputIsNumber(2).then(multipleByTwo).then(multipleByTwo).then(multipleByThree).then(res => console.log(res)).catch(err => console.log(err));
-afterPromise()
+// beforePromise().then(res => console.log(res));
+// inputIsNumber(2).then(multipleByTwo).then(multipleByTwo).then(multipleByThree).then(res => console.log(res)).catch(err => console.log(err));
+// afterPromise()
 // console.log("after promise");
