05796f98f92e14facf8e955add97ffaf605c2c83
Indra Putra <indrakoslab@gmail.com>
2018-07-20 11:16:13 +0700

    update js file with callback, promise and async/await

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 // ===============================================================
+// callback example
+const logName = (name) => {
+    console.log(`My Name Is ${name}`);
+}
+
+const insertCostumName = (callback) => {
+    var name = "Indra";
+    callback(name);
+}
+
+insertCostumName(logName);
+
+
+// ===============================================================
 // Promise basic example
-let condition = false;
+let condition = true;
 
 let myFirstPromise = new Promise((resolve, reject) => {
     if (condition) {
@@ -19,7 +33,7 @@ myFirstPromise
 
 // ===============================================================
 // Promise advanced example
-const isMomHappy = true;
+const isMomHappy = false;
 
 const willGetNewPhone = new Promise((resolve, reject) => {
     if (isMomHappy) {
@@ -42,8 +56,7 @@ const showOff = (phone) => {
 
 const askMom = () => {
     willGetNewPhone
-        .then(showOff)
-        .then(fullfilled => console.log(fullfilled))
+        .then(showOff).then(fullfilled => console.log(fullfilled))
         .catch(error => console.log(error.message));
 };
 
@@ -51,6 +64,68 @@ askMom();
 
 // ===============================================================
 // Promise advanced example - indra version
+
 const interviewerLovesYou = true;
 
-const willGetNewJob = new Promise(resolve)
+const willGetNewJob = new Promise((resolve, reject) => {
+    if (interviewerLovesYou) {
+        const jobs = {
+            posistion: "Software Engineer",
+            level: "Senior"
+        };
+        resolve(jobs);
+    } else {
+        const reason = new Error(
+            "you are not suitable working at our company");
+        reject(reason);
+    }
+});
+
+const acceptance = (jobs) => {
+    const message =
+        `Dear Mr. Indra,
+        We glad to see you to join our team as ${jobs.level} ${jobs.posistion}`;
+    return Promise.resolve(message);
+};
+
+const followUp = () => {
+    willGetNewJob
+        .then(acceptance).then(youhire => console.log(youhire))
+        .catch(error => console.log(error.message));
+};
+
+followUp();
+
+// ===============================================================
+// Async/await
+async function myAsyncFunction() {
+    return "The Value";
+}
+
+myAsyncFunction().then(returnedVal => console.log(returnedVal));
+
+// ===============================================================
+/*
+async function waiting() {
+    let promise = new Promise((resolve) => {
+        setTimeout(() => resolve("Times Up!"), 1000)
+    });
+    let result = await promise;
+    alert(result);
+}
+waiting();
+*/
+// ===============================================================
+const numberOrNot = (input) => {
+    return new Promise((resolve, reject)=>{
+        if(typeof input === "number"){
+            resolve("input is a number")
+        } else {
+            reject (Error("input is not number"))
+        }
+    })
+}
+
+numberOrNot(2).then(Response => console.log(Response)).catch(Error => console.log(Error))
+numberOrNot(3).then(Response => console.log(Response)).catch(Error => console.log(Error))
+numberOrNot(50).then(Response => console.log(Response)).catch(Error => console.log(Error));
